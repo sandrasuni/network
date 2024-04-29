e10507ce3098d4e779b5a2c8bafa959135ade8dd
sandrasuni <147145698+sandrasuni@users.noreply.github.com>
2024-04-29 02:21:05 -0800

    completed

---
+++ delspecificwd.sh
@@ -0,0 +1,19 @@
+
+if [ $# -ne 2 ]
+then
+echo " syntax is <$0><string><filename>" 
+exit 1
+fi
+term=`tty`
+exec<$2
+while read line
+do
+echo $line | grep $1>/dev/null
+echo $line | grep -w $1>/dev/null
+if [ $? -ne 0 ]
+then
+echo $line >>temp
+fi
+done
+exec<$term
+mv temp $2
